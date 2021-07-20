97dae89f1a49bd1371b1eded190a8a5c9634184e
Amanda Carvalho <amandamvcarvalho1989@gmail.com>
2021-07-20 13:52:20 -0300

    initial

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const Dashboard: React.FC = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Dashboard</Text>
+    </View>
+  );
+};
+
+export default Dashboard;
