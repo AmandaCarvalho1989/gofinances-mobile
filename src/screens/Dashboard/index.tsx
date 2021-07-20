be605065b60a0fbc89b0526e0aa867df12e4d960
Amanda Carvalho <amandamvcarvalho1989@gmail.com>
2021-07-20 17:30:27 -0300

    continue

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,68 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Container, Title } from "./styles";
+
+import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard } from "../../components/TransactionCard";
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  User,
+  Photo,
+  UserGretting,
+  UserName,
+  LogoutIcon,
+  HighlightCards,
+  Transactions,
+  Title,
+} from "./styles";
+
 export const Dashboard: React.FC = () => {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/53491128?v=4",
+              }}
+            />
+            <User>
+              <UserGretting>Olá,</UserGretting>
+              <UserName>Amanda</UserName>
+            </User>
+          </UserInfo>
+          <LogoutIcon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          title="Entrada"
+          type="up"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          title="Saída"
+          type="down"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 13 de abril"
+        />
+        <HighlightCard
+          title="Total"
+          type="total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionCard />
+      </Transactions>
     </Container>
   );
 };
